832e2f8fbed227a7c19dd259efe9fe4d5046396a
Eduardo Nascimento <edwardo.xavier@gmail.com>
2022-05-03 08:55:31 -0300

    update of code

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const connectToDatabase = require('./src/database/database');
 
 const port = 3000;
 const app = express();
 const route = require('./src/routes/paletas.route');
+
+connectToDatabase();
 
 app.use(cors());
 app.use(express.json());
